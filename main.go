fd703cc336bf4e2d0d7e5390078b961da5b13dee
or0986113303 <or0986113303@gmail.com>
2021-07-14 13:42:03 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/or0986113303/protocolbufferlearn"
+)
+
+func main() {
+	s := &pb.Student{
+		Name:   "Peng Jie",
+		Age:    24,
+		Gender: "Male",
+		Number: 99,
+	}
+	log.Println(
+		s.GetName(),
+		s.GetAge(),
+		s.GetGender(),
+		s.GetNumber(),
+	)
+}
